4014aa0023756f5be98ea401d1241bb1f0e1565b
glesiosantos <glesioss@gmail.com>
2023-10-23 04:53:53 -0300

    setting app and celebrate

--- src/shared/http/routers/home_route.ts
+++ src/shared/http/routers/home_route.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from 'express'
 
 export default (router: Router): void => {
-  router.get('/', (request: Request, response: Response) => response.json())
+  router.get('/', (request: Request, response: Response) => response.json({ message: 'Hello World' }))
 }
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,11 +1,2 @@
-import 'express-async-error'
-import express, { json } from 'express'
-import cors from 'cors'
-import routerSetup from '../../config/routes'
-
-const app = express()
-app.use(cors())
-app.use(json())
-routerSetup(app)
-
+import app from '../../config/app'
 app.listen(3000, () => console.log('Running app in port 3000'))
